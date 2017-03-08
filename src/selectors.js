30c0e856fcf1281dc96397bac125e1f2ec2bac6c
Marek Libra <mlibra@redhat.com>
2017-03-08 08:57:45 +0100

    Fix incorrect use of filter()

--- src/selectors.js
+++ src/selectors.js
@@ -2,6 +2,6 @@ import { getHostAddress, isSameHostAddress } from './helpers';
 
 export function getCurrentHost (hosts) {
   const currentHostAddress = getHostAddress();
-  const hostId = hosts.filter(hostId => currentHostAddress === hosts[hostId].address);
+  const hostId = Object.getOwnPropertyNames(hosts).filter(hostId => currentHostAddress === hosts[hostId].address);
   return hostId ? hosts[hostId] : undefined;
 }
